2a08bd1c3382c32e9efad6a112da9191c03cc3ab
hplaptop <7720585+nono-london@users.noreply.github.com>
2024-04-29 10:25:17 +0200

    all methods test

--- tests/test_api_gps.py
+++ tests/test_api_gps.py
@@ -33,3 +33,36 @@ async def test_get_gps_coordinates_with_insee():
     assert isinstance(gps_datas.get("lng"), float)
 
 
+@pytest.mark.asyncio
+async def test_batch_gps_coordinates():
+    """test the gps method with a List addresses that have postcode and city
+
+    """
+    my_postal_addresses: list = ["2 rue de la paix 75002 Paris",
+                                 "29 rue de la paix 75002 Paris",
+                                 ]
+    dvf_api = EtalabGpsApi()
+
+    gps_datas = await dvf_api.batch_gps_coordinates(postal_addresses=my_postal_addresses,
+                                                    )
+    for gps_data in gps_datas:
+        assert gps_data.get("city") is not None
+        assert isinstance(gps_data.get("lat"), float)
+        assert isinstance(gps_data.get("lng"), float)
+
+
+@pytest.mark.asyncio
+async def test_batch_gps_coordinates_with_insee():
+    """test the gps method with a List Tuples containing the address and the commune INSEE code
+    """
+    addresses_insees: list = [("2 rue de la paix 75002 Paris", '75102'),
+                              ("29 rue de la paix 75002 Paris", '75102')
+                              ]
+    dvf_api = EtalabGpsApi()
+
+    gps_datas = await dvf_api.batch_gps_coordinates(addresses_insees=addresses_insees
+                                                    )
+    for gps_data in gps_datas:
+        assert gps_data.get("city") is not None
+        assert isinstance(gps_data.get("lat"), float)
+        assert isinstance(gps_data.get("lng"), float)
